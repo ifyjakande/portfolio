8d345e79374a45eb9045aaa66fbcb63a91a901f7
Akande Ifeoluwa <akandeifeoluwa@gmail.com>
2025-01-18 00:01:57 +0100

    Update next.config.js for Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true, // Required for Cloudflare Pages
     remotePatterns: [
